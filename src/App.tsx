1be9bc4db9eb3dc9711110a83cd8d3eb4eb2e64d
Niklas Mårdby <niklas.mardby@gmail.com>
2025-03-04 13:16:01 +0100

    Update Readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import "./App.scss";
-import DisneyCharacterCard from "./components/DisneyCharacterCard/DisneyCharacterCard";
-import DisplayData from "./components/DisplayData/DisplayData";
-import LukeCard from "./components/LukeCard/LukeCard";
-import SWPeopleCard from "./components/SWPeopleCard/SWPeopleCard";
 
 function App() {
 	return (
 		<>
 			<h1>rocket</h1>
 			<p>a demo of useEffect</p>
-			<DisplayData />
-			{/* <LukeCard />
+			{/*<DisplayData />
+			 <LukeCard />
 			<SWPeopleCard id={2} />
 			<SWPeopleCard id={3} />
 			<SWPeopleCard id={22} />
